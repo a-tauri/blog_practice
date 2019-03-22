076503a73b18f1c5e586b4b97c3631138e56fb32
System Administrator <root@yoshimoto-masayuki-no-imac.local>
2019-03-23 07:58:33 +0900

    searchClass.php table 修正

--- functions/searchClass.php
+++ functions/searchClass.php
@@ -11,9 +11,9 @@ class searchClass
 		require_once './functions/db_manager.php';
 		$db = getDb();
 		// table名でカテゴリと記事本文をケース分け 不要
-		$stt = $db->prepare("SELECT * FROM :category WHERE id = :id");
+		$stt = $db->prepare("SELECT * FROM :table WHERE id = :id");
 	    $stt->bindValue(':id',$id);
-	    $stt->bindValue(':category',$id);
+	    $stt->bindValue(':table',$table);
 	    $stt->execute();
 	    $fet = $stt->fetch(PDO::FETCH_ASSOC);
 	    return $fet;
